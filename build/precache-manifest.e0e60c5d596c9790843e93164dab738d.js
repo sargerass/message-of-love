e0d55d720141a59cfe71cd29b4ab6bea11dd33eb
Guillermo Asto <guillermo.asto@avantica.net>
2020-09-05 03:59:17 -0500

    mjoera

---
+++ build/precache-manifest.e0e60c5d596c9790843e93164dab738d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b813b1954eea05fe25e0c4a0b7eee16e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a58646642d0ced62d33a",
+    "url": "/static/css/main.d5c421f5.chunk.css"
+  },
+  {
+    "revision": "2f5faf013329a1771e81",
+    "url": "/static/js/2.a68f123d.chunk.js"
+  },
+  {
+    "revision": "b9941b26ce7f87a25d1a3bc0d5ae97bc",
+    "url": "/static/js/2.a68f123d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a58646642d0ced62d33a",
+    "url": "/static/js/main.ab1a35c0.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "d094f5091e2ba42ec85a939899f066b4",
+    "url": "/static/media/problema.d094f509.jpg"
+  }
+]);
